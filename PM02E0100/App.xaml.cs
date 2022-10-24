8ba1a908f371641d3d611ce8796f8d89c460cbe7
yesman4_dev <56881149+yesman4-dev@users.noreply.github.com>
2022-10-23 21:09:26 -0600

    Agregar archivos de proyecto.

---
+++ PM02E0100/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PM02E0100.Views;
+
+namespace PM02E0100
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
